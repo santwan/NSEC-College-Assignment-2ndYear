3823a9a343d760d6342b405ea350e1b071e3dc3d
santwan <santwanpathak1@gmail.com>
2023-08-10 15:02:26 +0530

    Renaming & Adding two New Question
    ADDING TWO QUESTION
    TOWER OF HANOI
    QUENCE OPERATION

--- Question_Num_3.c
+++ Question_Num_3.c
@@ -1,77 +1,63 @@
+
 #include <stdio.h>
 
-#define MAX 10
+// Function to perform binary search
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;          // Leftmost index of the array
+    int right = n - 1;     // Rightmost index of the array
 
-int queue[MAX];
-int front = -1;
-int rear = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2; // Calculate the middle index
 
-void enqueue(int data) {
-  if (rear == MAX - 1) {
-    printf("Queue is full\n");
-    return;
-  }
+        if (arr[mid] == target) {
+            return mid; // Element found at the middle index
+        }
 
-  rear++;
-  queue[rear] = data;
-}
+        if (arr[mid] < target) {
+            left = mid + 1; // If target is greater, search the right half
+        } else {
+            right = mid - 1; // If target is smaller, search the left half
+        }
+    }
 
-int dequeue() {
-  if (front == rear) {
-    printf("Queue is empty\n");
-    return -1;
-  }
-
-  front++;
-  return queue[front];
-}
-
-void display() {
-  if (front == rear) {
-    printf("Queue is empty\n");
-    return;
-  }
-
-  int i;
-  for (i = front + 1; i <= rear; i++) {
-    printf("%d ", queue[i]);
-  }
-  printf("\n");
+    return -1; // Element not found in the array
 }
 
 int main() {
-  int choice;
-  int data;
+    int n, target;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
 
-  while (1) {
-    printf("1. Enqueue\n");
-    printf("2. Dequeue\n");
-    printf("3. Display\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    switch (choice) {
-      case 1:
-        printf("Enter data to enqueue: ");
-        scanf("%d", &data);
-        enqueue(data);
-        break;
-      case 2:
-        data = dequeue();
-        if (data == -1) {
-          printf("Queue is empty\n");
-        } else {
-          printf("Data dequeued: %d\n", data);
-        }
-        break;
-      case 3:
-        display();
-        break;
-      case 4:
-        return 0;
-      default:
-        printf("Invalid choice\n");
+    int arr[n];
+    printf("Enter the elements in sorted order: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-  }
+
+    printf("Enter the element to search for: ");
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1) {
+        printf("Element found at index %d.\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
 }
+
+// Explanation:
+
+// 1. We include the necessary header file `stdio.h` to use input/output functions in C.
+
+// 2. The `binarySearch` function is defined to perform binary search on a sorted array. It takes the array (`arr[]`), the number of elements in the array (`n`), and the target element to search for (`target`) as inputs and returns the index of the target element if found or -1 if not found.
+
+// 3. Inside the `binarySearch` function, we set up the left and right pointers to represent the indices of the leftmost and rightmost elements in the array.
+
+// 4. We use a `while` loop to perform the binary search until `left` is less than or equal to `right`. If `left` becomes greater than `right`, the target element is not present in the array, and we return -1.
+
+// 5. Inside the loop, we calculate the middle index (`mid`) using integer division and update the pointers accordingly based on the value of the element at the middle index compared to the target.
+
+// 6. If the element at `mid` is equal to the target, we have found the element, and
--- Question_Num_4.c
+++ Question_Num_4.c
@@ -1,63 +1,43 @@
+//TOWER OF HANOI 
 
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to perform binary search
-int binarySearch(int arr[], int n, int target) {
-    int left = 0;          // Leftmost index of the array
-    int right = n - 1;     // Rightmost index of the array
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2; // Calculate the middle index
-
-        if (arr[mid] == target) {
-            return mid; // Element found at the middle index
-        }
-
-        if (arr[mid] < target) {
-            left = mid + 1; // If target is greater, search the right half
-        } else {
-            right = mid - 1; // If target is smaller, search the left half
-        }
+void TowerOfHanoi( int n , char source , char aux , char dest){
+    //If Number of disk  Zero and also Base Condition 
+    if(n==0){
+        return ;
     }
+    
+    // Recursive Function 
+    TowerOfHanoi(n-1, source , aux , dest) ;
+    printf("Move Disk From %d from %c to %c \n", n , source , dest ) ;
+    TowerOfHanoi( n-1 , source , aux , dest ) ;
 
-    return -1; // Element not found in the array
 }
 
 int main() {
-    int n, target;
-    printf("Enter the number of elements in the array: ");
+
+    int n;
+    printf("Enter the number of disks: ");
     scanf("%d", &n);
 
-    int arr[n];
-    printf("Enter the elements in sorted order: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+    TowerOfHanoi(n, 'A', 'B', 'C');
 
-    printf("Enter the element to search for: ");
-    scanf("%d", &target);
-
-    int result = binarySearch(arr, n, target);
-
-    if (result != -1) {
-        printf("Element found at index %d.\n", result);
-    } else {
-        printf("Element not found in the array.\n");
-    }
-
-    return 0;
+return 0;
 }
 
-// Explanation:
 
-// 1. We include the necessary header file `stdio.h` to use input/output functions in C.
 
-// 2. The `binarySearch` function is defined to perform binary search on a sorted array. It takes the array (`arr[]`), the number of elements in the array (`n`), and the target element to search for (`target`) as inputs and returns the index of the target element if found or -1 if not found.
 
-// 3. Inside the `binarySearch` function, we set up the left and right pointers to represent the indices of the leftmost and rightmost elements in the array.
 
-// 4. We use a `while` loop to perform the binary search until `left` is less than or equal to `right`. If `left` becomes greater than `right`, the target element is not present in the array, and we return -1.
 
-// 5. Inside the loop, we calculate the middle index (`mid`) using integer division and update the pointers accordingly based on the value of the element at the middle index compared to the target.
 
-// 6. If the element at `mid` is equal to the target, we have found the element, and
+
+
+
+
+
+
+
+
+
